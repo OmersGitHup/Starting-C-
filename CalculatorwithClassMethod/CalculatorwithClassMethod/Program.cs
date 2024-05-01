bf96de0cb56b0b3dcd04c6f8e10a5f085c20647d
OmersGitHup <117158464+OmersGitHup@users.noreply.github.com>
2024-05-01 19:09:35 +0300

    Add files via upload

---
+++ CalculatorwithClassMethod/CalculatorwithClassMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalculatorwithClassMethod;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Yapmak İstediğiniz İşlemin Sembolünü Yazınız");
+string key=Console.ReadLine();
+calculator cal = new calculator();
+int x, y;
+Console.WriteLine("Birinci Sayıyı Giriniz");
+x=Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("İkinci Sayıyı Giriniz");
+y=Convert.ToInt16(Console.ReadLine());
+
+if (key == "+")
+{
+    cal.Sum(x,y);
+}else if (key == "*")
+{
+    cal.time(x,y);
+}else if(key == "/")
+{
+    cal.div(x,y);
+}else if (key == "-"){
+    cal.sub(x,y);
+
+}
+else
+{
+    Console.WriteLine("Yanlış Sembol ve ya Sayı Girdiniz");
+}
---
+++ CalculatorwithClassMethod/CalculatorwithClassMethod/calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorwithClassMethod
+{
+    internal class calculator
+    {
+        public int Sum(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public int time(int x, int y)
+        {
+            int result = x * y;
+            Console.WriteLine   (result);
+            return result;
+        }
+        public int sub(int x, int y)
+        {
+            int result = x - y;
+            Console.WriteLine(result);
+            return (result);
+        }
+        public int div(int x, int y)
+        {
+            int result = x / y;
+            Console.WriteLine(result);
+            return (result);
+        }
+
+    }
+}
