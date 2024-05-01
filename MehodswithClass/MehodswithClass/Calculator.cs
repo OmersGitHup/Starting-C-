f3af36c27ca4b414978f6819e0a52fe01120e0e3
OmersGitHup <117158464+OmersGitHup@users.noreply.github.com>
2024-05-01 19:09:03 +0300

    Add files via upload

---
+++ MehodswithClass/MehodswithClass/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MehodswithClass
+{
+    internal class Calculator
+    {
+        public int sum(int n1,int n2)
+        {
+            int n3=n1+n2;
+            Console.WriteLine(n3);
+            return n3;
+        }
+
+        public int xQuare(int x)
+        {
+            int a2 = x * x;
+            Console.WriteLine("Karesi: "+a2);
+            
+            return a2; // x karesini döndür
+        }
+
+    }
+}
---
+++ MehodswithClass/MehodswithClass/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MehodswithClass;
+
+Console.WriteLine("Hello, World!");
+
+
+Calculator cal= new Calculator();
+cal.sum(3,5);
+cal.sum(4,5);
+
+cal.xQuare(5);
+
