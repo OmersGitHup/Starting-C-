36cf4fe48d8716e5d4b5e3e5a1e65ce183afdfc2
OmersGitHup <117158464+OmersGitHup@users.noreply.github.com>
2024-05-01 19:10:32 +0300

    Add files via upload

---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Methods2;
+
+Console.WriteLine("Name-Club");
+string blg;
+blg= Console.ReadLine();
+
+
+ Students std=new Students("Statu:"+blg);
---
+++ Methods2/Methods2/Students.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    internal class Students
+    {
+        public Students(string info)
+        {
+            Console.WriteLine(info);
+
+        }
+    }
+}
